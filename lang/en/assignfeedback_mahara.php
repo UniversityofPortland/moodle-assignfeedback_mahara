c4acb628fe43d1eadb33f60f056094067da819c1
Philip Cali <philip.cali@gmail.com>
2013-06-24 00:18:59 -0400

    Initial commit on the Feedback plugin

---
+++ lang/en/assignfeedback_mahara.php
@@ -0,0 +1,27 @@
+<?php
+
+// General
+$string['pluginfile'] = 'Mahara feedback';
+$string['pluginname'] = 'Mahara feedback';
+$string['typemahara'] = 'Mahara feedback';
+
+// Settings
+$string['enabled'] = 'Mahara feedback';
+$string['enabled_help'] = 'If enabled, teachers can submit grades to Mahara';
+
+// Error
+$string['err_save_setting'] = 'Failed to save Mahara site setting. Please try again.';
+$string['err_selected_host'] = 'Failed to find a suitable Mahara site. Please try again.';
+$string['err_mnetclient'] = 'The following RPC failures: {$a}';
+$string['noviewscreated'] = 'You have not created any pages in {$a}.';
+$string['noviewsfound'] = 'No matching pages found in {$a}.';
+$string['nomaharahostsfound'] = 'No mahara hosts found.';
+
+$string['clicktopreview'] = 'click to preview in full-size popup';
+$string['clicktoselect'] = 'click to select page';
+$string['preview'] = 'Preview';
+$string['selectedview'] = 'Submitted Page';
+$string['selectmaharaview'] = 'Select one of your {$a->name} portfolio pages from this complete list, or <a href="{$a->jumpurl}">click here</a> to visit {$a->name} and create a page right now.';
+$string['title'] = 'Title';
+$string['views'] = 'Pages';
+$string['viewsby'] = 'Pages by {$a}';
---
+++ locallib.php
@@ -0,0 +1,235 @@
+<?php
+
+class assign_feedback_mahara extends assign_feedback_plugin {
+
+  /**
+   * @see parent
+   * @return string
+   */
+  public function get_name() {
+    return get_string('pluginfile', 'assignsubmission_mahara');
+  }
+
+  /**
+   * @see parent
+   */
+  public function is_empty(stdClass $grade) {
+    return false;
+  }
+
+  /**
+   * @see parent
+   */
+  public function get_settings(MoodleQuickForm $form) {
+    $plugin = $this->assignment->get_submission_plugin_by_type('mahara');
+    if (empty($plugin)) {
+      $form->hardFreeze('assignfeedback_mahara_enabled');
+    } else {
+      $form->disabledIf('assignfeedback_mahara_enabled', 'assignsubmission_mahara_enabled');
+    }
+  }
+
+  /**
+   * @see parent
+   */
+  public function save(stdClass $grade, stdClass $data) {
+    if ($release = $this->prepare_release($grade)) {
+      $plugin = $this;
+      return $release
+        ->map(
+          function($tuple) use ($plugin, $data) {
+            list($mahara, $event, $portfolio) = $tuple;
+
+            $outcomes = $plugin->process_outcomes_from_form($event->grade, $data);
+            return $plugin->complete_release($mahara, $event, $portfolio, $outcomes);
+          })
+        ->getOrElse(true);
+    }
+
+    return true;
+  }
+
+  /**
+   * Prepares the release scenario
+   *
+   * @param stdClass $grade
+   * @return Model_Option|null
+   */
+  private function prepare_release($grade) {
+    $event = new stdClass;
+    $event->assignment = $this->assignment;
+    $event->grade = $grade;
+    $event->submission = $this->get_submission_for_grade($grade);
+
+    $mahara = $event->assignment->get_submission_plugin_by_type('mahara');
+
+    if ($submitted = $mahara->get_portfolio_record($event->submission) ) {
+      return $mahara
+        ->get_service()
+        ->get_local_portfolio($submitted->portfolio)
+        ->map(function($portfolio) use ($mahara, $event) {
+          return array($mahara, $event, $portfolio);
+        });
+    }
+
+    return null;
+  }
+
+  /**
+   * Completes the commone release scenario
+   *
+   * @param mixed $mahara
+   * @param stdClass $event
+   * @param stdClass $portfolio
+   * @param array $outcomes
+   * @return boolean
+   */
+  public function complete_release($mahara, $event, $portfolio, $outcomes) {
+    return $mahara
+      ->get_service()
+      ->request_release_submitted_view(
+        $event->grade->grader,
+        $portfolio->page,
+        $outcomes
+      )
+      ->withRight()
+      ->each(
+        function() use ($event) {
+          events_trigger('assign_mahara_grade_submitted', $event);
+        })
+      ->isRight();
+  }
+
+  /**
+   * @see parent
+   */
+  public function supports_quickgrading() {
+    return true;
+  }
+
+  /**
+   * @see parent
+   */
+  public function save_quickgrading_changes($userid, $grade) {
+    if ($release = $this->prepare_release($grade)) {
+      $plugin = $this;
+      return $release
+        ->map(
+          function($tuple) use ($grade, $plugin) {
+            list($mahara, $event, $portfolio) = $tuple;
+
+            $outcomes = $plugin->process_outcomes_from_quickgrading($grade);
+            return $plugin->complete_release($mahara, $event, $portfolio, $outcomes);
+          })
+        ->getOrElse(true);
+    }
+    return true;
+  }
+
+  /**
+   * Get user grading info
+   *
+   * @param $grade
+   * @return grading_info
+   */
+  public function get_user_grade_info($grade) {
+    return $grading_info = grade_get_grades(
+      $this->assignment->get_course()->id,
+      'mod',
+      'assign',
+      $this->assignment->get_instance()->id,
+      $grade->userid
+    );
+  }
+
+  /**
+   * Process outcome data from quick grading
+   *
+   * @param $grade
+   * @return array
+   */
+  public function process_outcomes_from_quickgrading($grade) {
+    $grading_info = $this->get_user_grade_info($grade);
+
+    $viewoutcomes = array();
+    if (!empty($grading_info->outcomes)) {
+      foreach ($grading_info->outcomes as $outcomeid => $outcome) {
+        $newoutcome_name = "outcome_{$outcomeid}_{$grade->userid}";
+        $oldoutcome = $outcome->grades[$grade->userid]->grade;
+        $newoutcome = optional_param($newoutcome_name, -1, PARAM_INT);
+
+        $scale = make_grades_menu(-$outcome->scaleid);
+        if ($oldoutcome == $newoutcome || !isset($scale[$newoutcome])) {
+          continue;
+        }
+
+        foreach ($scale as $k => $v) {
+          $scale[$k] = array('name' => $v, 'value' => $k);
+        }
+
+        $viewoutcomes[] = array(
+          'name' => $outcome->name,
+          'scale' => $scale,
+          'grade' => $newoutcome,
+        );
+      }
+    }
+
+    return $viewoutcomes;
+  }
+
+  /**
+   * Process outcome data from a form
+   *
+   * @param $grade
+   * @param stdClass $formdata
+   * @return array
+   */
+  public function process_outcomes_from_form($grade, $formdata) {
+    $grading_info = $this->get_user_grade_info($grade);
+    $viewoutcomes = array();
+
+    if (!empty($grading_info->outcomes)) {
+      foreach ($grading_info->outcomes as $index => $outcome) {
+        $name = "outcome_$index";
+        $oldoutcome = $outcome->grades[$grade->userid]->grade;
+        $scale = make_grades_menu(-$outcome->scaleid);
+
+        if (
+          !isset($formdata->{$name}[$grade->userid]) ||
+          $oldoutcome == $formdata->{$name}[$grade->userid] ||
+          !isset($scale[$formdata->{$name}[$grade->userid]])
+        ) {
+          continue;
+        }
+
+        foreach ($scale as $k => $v) {
+          $scale[$k] = array('name' => $v, 'value' => $k);
+        }
+
+        $viewoutcomes[] = array(
+          'name' => $outcome->name,
+          'scale' => $scale,
+          'grade' => $formdata->{$name}[$grade->userid],
+        );
+      }
+    }
+
+    return $viewoutcomes;
+  }
+
+  /**
+   * Gets the submission for the grade
+   *
+   * @param stdClass $grade
+   * @return stdClass $submission
+   */
+  public function get_submission_for_grade($grade) {
+    global $DB;
+
+    return $DB->get_record('assign_submission', array(
+      'assignment' => $this->assignment->get_instance()->id,
+      'userid' => $grade->userid,
+    ));
+  }
+}
