d121daf67a70b4852c63c05927ad0a952b1aacc9
fellowapeman <box@up.edu>
2013-07-22 15:04:30 -0700

    Update version.php

--- version.php
+++ version.php
@@ -3,3 +3,7 @@
 $plugin->version = 2013061200;
 $plugin->requires = 2012061700;
 $plugin->component = 'assignfeedback_mahara';
+
+$plugin->dependencies = array(
+    'local_mahara' => ANY_VERSION
+);
