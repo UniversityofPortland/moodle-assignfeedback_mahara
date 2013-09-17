33c21b2bd4ef782af367db4d20ca37f337ad703b
fellowapeman <box@up.edu>
2013-09-17 11:37:09 -0700

    Update version.php

--- version.php
+++ version.php
@@ -2,6 +2,8 @@
 
 $plugin->version = 2013061200;
 $plugin->requires = 2012062500;
+$plugin->release = '1.0rc (Build: 2013061200)';
+$plugin->maturity = MATURITY_RC;
 $plugin->component = 'assignfeedback_mahara';
 
 $plugin->dependencies = array(
